5b2f8f58a645f632b41c83d983de872492116236
smartinvestItn1 <SmartinvetItn@gmail.com>
2026-04-29 16:51:38 +0100

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -1,5 +1,16 @@
 const user = localStorage.getItem("loggedInUser");
+const sessionId = localStorage.getItem("sessionId");
 
-if (!user) {
+let sessions = JSON.parse(localStorage.getItem("activeSessions")) || [];
+
+let validSession = sessions.find(s =>
+  s.username === user && s.sessionId === sessionId
+);
+
+if (!user || !sessionId || !validSession) {
+  localStorage.removeItem("loggedInUser");
+  localStorage.removeItem("sessionId");
+  localStorage.removeItem("role");
+
   window.location.replace("login.html");
 }
