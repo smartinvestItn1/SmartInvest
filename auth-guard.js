2f63dfd4eb614e16defe99f0ef08dc2a2d3ba5a9
smartinvestItn1 <SmartinvetItn@gmail.com>
2026-05-23 13:28:46 +0100

    Delete auth-guard.js

--- auth-guard.js
+++
@@ -1,6 +0,0 @@
-const currentUser = sessionStorage.getItem("loggedInUser");
-
-if (!currentUser || currentUser === "null" || currentUser === "undefined") {
-  sessionStorage.removeItem("loggedInUser");
-  window.location.replace("login.html");
-}
