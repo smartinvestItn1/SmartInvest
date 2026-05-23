575a4a481fbb815b8d966f3e4ae8690b4ff63238
smartinvestItn1 <SmartinvetItn@gmail.com>
2026-05-23 12:25:03 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,35 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
+// IMPORT FIREBASE
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+// YOUR FIREBASE CONFIG
 const firebaseConfig = {
+
   apiKey: "AIzaSyDvQajNkcsd-VvnBsiX0OI_cWOdegQC9JY",
+
   authDomain: "smartinvest-web-dc8b7.firebaseapp.com",
+
+  databaseURL: "https://smartinvest-web-dc8b7-default-rtdb.firebaseio.com",
+
   projectId: "smartinvest-web-dc8b7",
-  storageBucket: "smartinvest-web-dc8b7.appspot.com",
+
+  storageBucket: "smartinvest-web-dc8b7.firebasestorage.app",
+
   messagingSenderId: "865666898515",
+
   appId: "1:865666898515:web:b83ed75380a8ab0c5fc4a6"
 };
 
+
+// INITIALIZE FIREBASE
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+
+
+// EXPORT SERVICES
+export const auth = getAuth(app);
+
+export const db = getDatabase(app);
